a24721724178bd6b6789c00bb8380b47abb58505
Lunarixus <Nathan@lunarixus.dev>
2021-12-20 17:09:28 +0000

    beyond0lte: Initial skeleton tree
    Signed-off-by: Lunarixus <Nathan@lunarixus.dev>

---
+++ lineage_beyond0lte.mk
@@ -0,0 +1,35 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# Inherit from troika device
+$(call inherit-product, device/motorola/troika/device.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := beyond0lte
+PRODUCT_NAME := lineage_beyond0lte
+PRODUCT_BRAND := Samsung
+PRODUCT_MODEL := Galaxy S10e
+PRODUCT_MANUFACTURER := Samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
